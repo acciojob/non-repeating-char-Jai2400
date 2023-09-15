4b1b751d9e13e638a3f12fd5a4abdc32f45ca03b
Jai2400 <117900074+Jai2400@users.noreply.github.com>
2023-09-15 19:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    if (str.length === 0) return null;
+    
+    let charCount = {};
+    
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str[i]]) {
+            charCount[str[i]]++;
+        } else {
+            charCount[str[i]] = 1;
+        }
+    }
+    
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str[i]] === 1) {
+            return str[i];
+        }
+    }
+    
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
